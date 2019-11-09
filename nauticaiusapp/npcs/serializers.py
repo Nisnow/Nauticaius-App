b0a77e9d12f504f582cb4d10bb91425f5fcaffe5
Nisnow <catherine.guevara2001@gmail.com>
2019-11-09 01:48:42 -0800

    Add field for NPC id

--- nauticaiusapp/npcs/serializers.py
+++ nauticaiusapp/npcs/serializers.py
@@ -5,4 +5,4 @@ class NpcSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Npc
-        fields = ('name', 'race', 'class_field', 'age', 'height', 'alignment', 'affiliation', 'first_appearance', 'str', 'dex', 'con', 'int', 'wis', 'cha', 'description')
+        fields = ('id', 'name', 'race', 'class_field', 'age', 'height', 'alignment', 'affiliation', 'first_appearance', 'str', 'dex', 'con', 'int', 'wis', 'cha', 'description')
